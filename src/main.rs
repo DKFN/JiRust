28edcd2e18245c8bfacc291b21c76c47a8bd6a8b
valere.tetelin <valere.tetelin@gmail.com>
2018-02-03 17:34:04 +0100

    JiRust is now working only thru CLI

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,66 @@
 extern crate reqwest;
 extern crate hyper;
 extern crate base64;
-extern crate rpassword;
+extern crate clap;
 
-use std::io;
 use base64::encode;
 
 fn main() {
-    // Script variables
-    let mut jira_host = String::new();
-    let mut jira_mail = String::new();
-    let mut jira_project = String::new();
+    let cli_conf = clap::App::new("JiRust")
+        .version("0.1.0")
+        .author("Valere Tetelin - Deadly :3 -")
+        .about("Formats your Jira issues to the TODO file for EPITA")
+        .arg(clap::Arg::with_name("JIRA_HOST")
+                 .short("h")
+                 .long("host")
+                 .takes_value(true)
+                 .required(true)
+                 .help("Your JIRA HOST protocol://adress:port/"))
+        .arg(clap::Arg::with_name("JIRA_MAIL")
+                 .short("l")
+                 .long("login")
+                 .takes_value(true)
+                 .required(true)
+                 .help("Your JIRA mail"))
+        .arg(clap::Arg::with_name("JIRA_PASS")
+                 .short("p")
+                 .long("pass")
+                 .takes_value(true)
+                 .required(true)
+                 .help("Your JIRA pass"))
+        .arg(clap::Arg::with_name("JIRA_PROJECT_ID")
+                 .short("i")
+                 .long("project_id")
+                 .takes_value(true)
+                 .required(true)
+                 .help("Your JIRA project"))
+        .get_matches();
 
-    println!("Welcome to JiRust (unstable) made by Bataillion APPING");
-    println!("We will ask you your atlassian email and password for processing requests");
-    println!("Please note we do not save them in any way");
-    println!("If you have questions about usage for this script please email valere.tetelin@gmail.com");
+    let jira_host = cli_conf.value_of("JIRA_HOST").unwrap();
+    let jira_mail = cli_conf.value_of("JIRA_MAIL").unwrap();
+    let jira_pass = cli_conf.value_of("JIRA_PASS").unwrap();
+    let jira_project = cli_conf.value_of("JIRA_PROJECT_ID").unwrap();
 
-    println!("Please enter the adress of your Jira (with full protocol and port)");
-    println!("Format is :: protocol://adresss:port/");
-
-    io::stdin().read_line(&mut jira_host).expect("Unable to read line");
-
-    println!("Please enter your Jira email :");
-
-    io::stdin().read_line(&mut jira_mail).expect("Unable to read line");
-
-    println!("Please enter your Jira password");
-
-    let jira_pass = rpassword::prompt_password_stdout("->")
-        .expect("Unable to catch the password");
-
-    // io::stdin().read_line(&mut jira_pass).expect("Unable to read line");
-
-    println!("Please enter the identifier of the project you wish to import (UPPERCASE)");
-
-    io::stdin().read_line(&mut jira_project).expect("Unable to read line");
+    println!("Welcome to JiRust !");
 
     println!("We are now trying to talk with your Jira's API .......");
 
     jira_get_issues(jira_host, jira_mail, jira_pass, jira_project);
 }
 
-fn jira_get_issues(jira_host: String,
-                   jira_mail: String,
-                   jira_pass: String,
-                   jira_project: String)
+fn jira_get_issues(jira_host: &str,
+                   jira_mail: &str,
+                   jira_pass: &str,
+                   jira_project: &str)
 {
-    let mut host = jira_host.to_owned();
-    let mut pass = jira_pass.to_owned();
-
-    // pass.pop();
-    host.pop();
-
-    let fin = format!("{}{}{}", host, "rest/api/2/search?jql=project=", jira_project);
+    let fin = format!("{}{}{}", jira_host, "rest/api/2/search?jql=project=", jira_project);
 
     println!("Targetting URL : {}", fin);
     let client = reqwest::Client::new();
 
     let mut credentials = jira_mail.to_owned();
-    credentials.pop();
     credentials.push_str(":");
-    credentials.push_str(&*pass);
+    credentials.push_str(jira_pass);
 
     let cred_hash = encode(&credentials);
     println!("Using auth hash : {}", cred_hash);
